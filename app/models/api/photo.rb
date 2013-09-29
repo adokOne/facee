8fe996df17fadb1ec64259200ef926210f3e6a83
adok <adok@ukr.net>
2013-09-29 13:26:12 +0300

    fixed

--- app/models/api/photo.rb
+++ app/models/api/photo.rb
@@ -6,7 +6,7 @@ class Photo
   field          :app_user_id, :type => Moped::BSON::ObjectId
   field          :album_id   , :type => Moped::BSON::ObjectId
   field          :bd         , :type => Integer,  :default => Time.now.to_i
-  field          :description_payed, :type  => Boolean, :default => false,
+  field          :description_payed, :type  => Boolean, :default => false
   field          :gender, :type => Integer,:default => 1
   auto_increment :id 
 

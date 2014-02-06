fbb2b4699b543bd88266c5d9690f8182dd55dab9
adok <adok@ukr.net>
2014-02-06 11:06:23 +0200

    fixed

--- app/controllers/api/pictures_controller.rb
+++ app/controllers/api/pictures_controller.rb
@@ -6,7 +6,7 @@ class Api::PicturesController < Api::ApiController
 
   def list
     total = ::Photo.where(:app_user=>$current_user.id).count
-    items = ::Photo.where(:app_user=>$current_user.id).paginate(:per_page => Settings.app.photos_limit, :page => params[:page]).map(&:to_strim)
+    items = ::Photo.where(:app_user=>$current_user.id).map(&:to_strim)
   	@result = {:total=>total,:items=>items}
   end
 
